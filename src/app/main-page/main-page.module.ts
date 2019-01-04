ff97e4fe75d7e3f278485be0e2b3e5a0c7b71cb8
AnhDT2511 <s2.25111211@gmail.com>
2019-01-05 01:24:10 +0700

    remove module

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageRoutingModule } from './main-page-routing.module';
 
-import { MainPageComponent } from './main-page.component';
 import { ListCourseComponent } from './list-course/list-course.component';
 
 @NgModule({
   declarations: [
-    MainPageComponent,
     ListCourseComponent
   ],
   imports: [
